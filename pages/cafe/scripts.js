523f215c46046087b9d49b2a1801e561b9bcf92c
Spencer Schiminsky <89555522+Truckerbug@users.noreply.github.com>
2023-10-13 09:10:30 -0700

    Update scripts.js

--- pages/cafe/scripts.js
+++ pages/cafe/scripts.js
@@ -17,6 +17,27 @@
      }
 } */
 
+$(document).ready(function(e) {
+    var refresher = setInterval("update_content();",30000); // 30 seconds
+})
+
+function update_content(){
+
+    $.ajax({
+      type: "GET",
+      url: "index.php", // post it back to itself - use relative path or consistent www. or non-www. to avoid cross domain security issues
+      cache: false, // be sure not to cache results
+    })
+      .done(function( page_html ) {
+        alert("LOADED");
+    var newDoc = document.open("text/html", "replace");
+    newDoc.write(page_html);
+    newDoc.close();
+
+    });   
+
+}
+
 function favorite() {
     document.getElementById("favorite").innerHTML = "Favorited";
     if (localStorage.getItem("favorited")) {
